dc556bb44ed062ba4d5b191b17dc849d9ff15480
Priyanshu Singh <priy2032004anshu@gmail.com>
2024-02-21 23:33:19 +0530

    feb 21

---
+++ Constructor.py
@@ -0,0 +1,52 @@
+# class person:
+#     def __init__(self,n,o):
+#         self.name=n
+#         self.occ=o
+
+#     def info(self):
+#         print(f"im {self.name}and my job is {self.occ}")
+
+
+
+# a=person("priyanshu","HR")
+# b=person("alok","maneger")
+# a.info()
+# b.info()
+
+
+class car:
+    def __init__(self,n,p,c):
+
+
+
+        self.name=n
+        self.price=p
+        self.color=c
+    def info(self):
+        print(f"car name is {self.name}")
+        print(f"car price is {self.price}")
+        print(f"car color is {self.color}")
+a=car("TATA",897346,"RED")
+a.info()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
