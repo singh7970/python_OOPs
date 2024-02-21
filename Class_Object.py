f8060bbceafd3438c1bc4309803539d278dbdc0f
Priyanshu Singh <priy2032004anshu@gmail.com>
2024-02-21 23:31:56 +0530

    feb 21

---
+++ Class_Object.py
@@ -0,0 +1,18 @@
+# class person:#this is class 
+#     name="priyanshu"
+#     occupation="manager"
+# a=person()#this is Object
+# print(a.name)
+# print(a.occupation)
+
+class car:
+     name ="hundai"
+     price=1232434
+
+a=car()
+print(a.name)
+print(a.price)
+
+
+
+
